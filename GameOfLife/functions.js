49c887159e73b04bcf04cfbd55213d26c4f83505
Kenny Chen <kenchen10@berkeley.edu>
2018-01-18 13:33:53 -0800

    deleted seekingtext

--- GameOfLife/functions.js
+++ GameOfLife/functions.js
@@ -1,81 +1,103 @@
-var arrayWidth = windowWidth;
-var arrayHeight = windowHeight;
-var density = 10;
-var numColumns = arrayWidth / density;
-var numRows = arrayHeight / density;
+var c = 50;
+var sign = -.15;
+let bool;
 
-function make2DArray(columns, rows) {
-  //Creates an array of arrays with 'rows' number of rows and 'columns' number of columns.
-  var array = new Array(columns);
-  for (let i = 0; i < array.length; i++) {
-    array[i] = new Array(rows);
+function make2DArray(cols, rows) {
+  //creates a 2D array
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
   }
-  return array;
+  return arr;
 }
 
-function countNumNeighbors(array, x_current, y_current) {
-  //Computes number of cells that neighbor the current cell.
-  var sum = 0;
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      var column = (x_current + i + numColumns) %  numColumns;
-      var row = (y_current + i + numRows) %  numRows;
-      sum += array[column][row];
+function initializeArray(cols, rows) {
+  //creates an initial 2D array of 1 or 0 values
+  let grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
     }
   }
-  sum -= array[x_current, y_current];
+  return grid;
+}
+
+function showArray(grid) {
+  //shows grid of cells
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        noStroke();
+        rect(x, y, resolution - 1, resolution - 1);
+      } //else if (bool[i][j] == true) {
+      //   fill(20);
+      //   stroke(0);
+      //   rect(x, y, resolution - 1, resolution - 1);
+      // }
+    }
+  }
+}
+
+function computeNext(grid, next) {
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+}
+
+function trail(grid, next) {
+  computeNext(grid, next);
+  let bool = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state1 = grid[i][j];
+      let state2 = next[i][j];
+
+      if (state1 == 1 && state2 == 0) {
+        bool[i][j] = true;
+      } else {
+        bool[i][j] = false;
+      }
+    }
+  }
+}
+
+function countNeighbors(grid, x, y) {
+  //counts number of neighbors
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
   return sum;
 }
 
-function initializeCells() {
-  //Creates initial random cell locations.
-  var array = make2DArray(numColumns, numRows);
-  for (let i = 0; i < numColumns; i++) {
-    for (let j = 0; j < numRows; j++) {
-      array[i][j] = floor(random(2));
-    }
+//Color gradient background
+function gradient() {
+  if (c >= 125 || c <= 50) {
+    sign *= -1;
+    c += sign;
+  } else {
+    c += sign;
   }
-  return array;
-}
-
-function showCells(array) {
-  //Shows cells.
-  for (let i = 0; i < numColumns; i++) {
-    for (let j = 0; j < numRows; j++) {
-      var length = i * density;
-      var width = j * density;
-      if (array[i][j] == 1) {
-        fill(255);
-        stroke(0);
-        rect(length, width, density, density);
-      }
-    }
-  }
-}
-
-function updateCells(old_array, new_array) {
-  /* Computes the new array based on rules described by Conway:
-  1.Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-  2.Any live cell with two or three live neighbours lives on to the next generation.
-  3.Any live cell with more than three live neighbours dies, as if by overpopulation.
-  4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. */
-  for (let i = 0; i < numColumns; i++) {
-    for (let j = 0; j < numRows; j++) {
-      var array_value = array[i][j];
-      var sum = 0;
-      var num_neighbors = countNumNeighbors(old_array, i, j);
-
-      if (array_value == 0 && num_neighbors == 3) {
-        new_array[i][j] = 1;
-      }
-      else if (array_value == 1 && (num_neighbors < 2 || num_neighbors > 3)) {
-        new_array[i][j] = 0;
-      }
-      else {
-        new_array[i][j] = array_value;
-      }
-      }
-    }
-  }
-  return new_array;
+  background(c, 77, c);
 }
--- GameOfLife/sketch.js
+++ GameOfLife/sketch.js
@@ -1,12 +1,23 @@
+let grid;
+let cols;
+let rows;
+let resolution = 7;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	old_array = initializeCells();
+  createCanvas(windowWidth, windowHeight);
+  cols = round(width / resolution);
+  rows = round(height / resolution);
+  //create initial array of array with random 0 or 1 value
+  grid = initializeArray(cols, rows);
 }
 
 function draw() {
-	background(0);
-	showCells(old_array);
-	var new_array = make2DArray(numColumns, numRows);
-	new_array = updateCells(old_array, new_array);
-	old_array = new_array;
+  //background gradient
+  gradient();
+  //show array
+  showArray(grid);
+  //update array based on rules
+  let next = make2DArray(cols, rows);
+  trail(grid, next);
+  grid = next;
 }
